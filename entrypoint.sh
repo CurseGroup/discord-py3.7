fb7ad727eb58a2bc36efcf231cf6728d8deb2b38
Arnaud Lier <zeprofdecoding@gmail.com>
2020-11-07 23:11:12 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-sleep 2
-
 cd /home/container
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-pip3 install -r requirements.txt
-${MODIFIED_STARTUP}
+eval ${MODIFIED_STARTUP}
